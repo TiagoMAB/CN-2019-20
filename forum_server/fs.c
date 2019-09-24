c48ee29a347482497d04de78e4e3ff0081458c1c
TiagoMAB <tiago.barroso1@hotmail.com>
2019-09-24 17:24:25 +0100

    argument validation

--- forum_server/fs.c
+++ forum_server/fs.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+void error(int error) {
+    fprintf(stdout, "ERR: Format incorrect. Should be: ./user [-n FSIP] [-p FSport]\n");
+    exit(error);
+}
 
 int main(int argc, char **argv) {
 
+    int option, p = 0;
+    char *fsport = NULL;
+    
 
-    if (argc > 3) {
-        fprintf(stderr, "Invalid number of arguments\n");
-        exit(1);
+    while ((option = getopt (argc, argv, "n:p:")) != -1) {
+        switch (option)
+        {
+        case 'p':
+            if (p) error(1);
+            p = 1;
+            fsport = optarg;
+            break;
+        default:
+            break;
+        }
     }
+
+    if (optind < argc) error(1);
+
+    printf("%s %i\n", fsport, optind);
+
     return 0;
 }
--- user/user.c
+++ user/user.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+void error(int error) {
+    fprintf(stdout, "ERR: Format incorrect. Should be: ./user [-n FSIP] [-p FSport]\n");
+    exit(error);
+}
 
 int main(int argc, char **argv) {
 
+    int option, n = 0, p = 0;
+    char *fsip = NULL, *fsport = NULL;
+    
 
-    if (argc > 3) {
-        fprintf(stderr, "Invalid number of arguments\n");
-        exit(1);
+    while ((option = getopt (argc, argv, "n:p:")) != -1) {
+        switch (option)
+        {
+        case 'n':
+            if (n) error(1);
+            n = 1;
+            fsip = optarg;
+            break;
+        case 'p':
+            if (p) error(1);
+            p = 1;
+            fsport = optarg;
+            break;
+        default:
+            break;
+        }
     }
+
+    if (optind < argc) error(1);
+
+    printf("%s %s %i\n", fsip, fsport, optind);
+
     return 0;
 }
