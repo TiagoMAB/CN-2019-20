97b00c47827848501139b76e954e56f631927da0
TiagoMAB <tiago.barroso1@hotmail.com>
2019-09-24 15:53:04 +0100

    number of arguments validation

--- user/user.c
+++ user/user.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(int argc, char **argv) {
 
+
+    if (argc > 3) {
+        fprintf(stderr, "Invalid number of arguments\n");
+        exit(1);
+    }
     return 0;
 }
