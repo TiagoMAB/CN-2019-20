66b436342e7583c3ca894229298903951e9b5b72
TiagoMAB <tiago.barroso1@hotmail.com>
2019-10-10 15:26:34 +0100

    reg done

--- fs/fs.c
+++ fs/fs.c
@@ -11,6 +11,8 @@
 
 #define PORT "58020"
 
+enum options { REG, LTP, PTP, LQU, GQU, QUS, ANS };
+
 void error(int error) {
 
     switch (error) {
@@ -31,28 +33,107 @@ void error(int error) {
     }
 }
 
-char* readSocket(int fdUDP, char* buffer, struct sockaddr_in addr, socklen_t addrlen) {
+char* readMessageUDP(int fdUDP, char* buffer, struct sockaddr_in *addr, socklen_t *addrlen) {
 
     int size = 1, data;
 
     buffer = (char*) malloc(sizeof(char)*size);
-    addrlen = sizeof(addr);
+    *addrlen = sizeof(addr);
 
-    data = recvfrom(fdUDP, buffer, size, MSG_PEEK, (struct sockaddr*)&addr, &addrlen);
+    data = recvfrom(fdUDP, buffer, size, MSG_PEEK, (struct sockaddr*)addr, addrlen);
     while (data == size) {
         free(buffer);
         size *= 2;
         buffer = (char*) malloc(sizeof(char)*size);
-        data = recvfrom(fdUDP, buffer, size, MSG_PEEK, (struct sockaddr*)&addr, &addrlen);
+        data = recvfrom(fdUDP, buffer, size, MSG_PEEK, (struct sockaddr*)addr, addrlen);
     }
     buffer[data] = '\0';
     
     return buffer;
 }
 
+void sendMessageUDP(int fdUDP, char* buffer, struct sockaddr_in addr, socklen_t addrlen) {
+
+    int n;
+    printf("%s - %ld", buffer, strlen(buffer));
+    addrlen = sizeof(addr);
+    n = sendto(fdUDP, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, addrlen);
+    if (n == -1) error(2);
+    printf("Message sent: %s", buffer);
+}
+
+int checkProtocol(char* token) {
+
+    char *opts[] = { "REG", "LTP", "PTP", "LQU", "GQU", "QUS", "ANS" };
+    int i;
+
+    for (i = 0; i < 7; i++) {
+        if (!strcmp(token, opts[i])) {
+            return i;
+        }
+    }
+    return i;
+}
+
+char* reg(char* buffer) {
+
+    char* token = strtok(NULL, "\n"); 
+
+    if (token != NULL && strlen(token) == 5) {
+        for (int i = 0; i < 5; i++) {
+            if (token[i] < '0' || token[i] > '9') {
+                return "RGR NOK\n";
+            }
+        }
+        return "RGR OK\n";
+    }
+    return "RGR NOK\n";
+}
+
 char* handleMessage(char* buffer) {
 
-    
+    char* token = NULL;
+    int result = -1;
+
+    token = strtok(buffer, " \n"); 
+
+    if (token != NULL) {
+        result = checkProtocol(token);
+    }
+
+    switch (result) {
+        case REG:
+            return reg(buffer);
+            break;
+
+        case LTP:
+        //    return ltp();
+            break;
+
+        case PTP:
+        //    return ptp();
+            break;
+
+        case LQU:
+        //    return lqu();
+            break;
+
+        case GQU:
+        //    return gqu();
+            break;
+
+        case QUS:
+        //    return qus();
+            break;
+
+        case ANS:
+        //    return ans();
+            break;
+
+        default:
+            return "ERR\n";
+            break;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -90,12 +171,12 @@ int main(int argc, char **argv) {
     if (bind(fdUDP, resUDP->ai_addr, resUDP->ai_addrlen) == -1) error(2);
 
     while (1) { 
-        buffer = readMessageUDP(fdUDP, buffer, addr, addrlen);
-        buffer = handleMessage(buffer);
-        sendMessageUDP();
-        printf("| %s %ld\n", buffer, strlen(buffer));
+        buffer = readMessageUDP(fdUDP, buffer, &addr, &addrlen);
+        sendMessageUDP(fdUDP, handleMessage(buffer), addr, addrlen);
         free(buffer);
+        break;
     }
+    freeaddrinfo(resUDP);
     close(fdUDP);
     return 0;
 }
